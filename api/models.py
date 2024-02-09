afa9d8d0200153719132d1cca33e9198c997daeb
Andrew Xu <andr3w.xu@gmail.com>
2024-02-08 19:06:23 -0500

    initial commit

---
+++ api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Post.objects.filer(user_id="") ...
+
+# Create your models here.
+class Note(models.Model):
+  # user_id = models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+  title = models.CharField(max_length=50, unique=True)
+  date_created = models.DateTimeField(auto_now_add=True)
+  text = models.TextField(max_length=5000)
+  url = models.CharField(max_length=500)
---
+++ api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import NoteSerializer
+from .models import Note
+
+# Create your views here.
+class NoteView(generics.CreateAPIView): # inherits
+  queryset = Note.objects.all()
+  serializer_class = NoteSerializer
